6c3992ff118508c2a3cb29f767739b0c49f0e8d2
Mateusz Kretowski <matkretkurs@gmail.com>
2023-05-02 01:21:42 +0200

    Add form in contact view

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
 
 const app = express();
 
@@ -8,6 +9,38 @@ app.engine('hbs', hbs()); //app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: 
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false })); //x-www-form-urlencoded
+app.use(express.json()); //form-data
+
+// multer settings
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/uploads/'); // catalogue for uploaded files
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname); // file name on server
+    },
+  }),
+  fileFilter: function (req, file, cb) {
+    const acceptedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
+    const fileExtension = path.extname(file.originalname).toLowerCase();
+    if (!acceptedExtensions.includes(fileExtension)) {
+      return cb(null, false);
+    }
+    cb(null, true); //null - no errors, true - file accepted
+  },
+});
+
+app.post('/contact/send-message', upload.single('file'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message && req.file) {
+    res.render('contact', { isSent: true, image: req.file.filename });
+  } else {
+    res.render('contact', { isError: true });
+  }
+});
 
 app.get('/', (req, res) => {
   res.render('index');
